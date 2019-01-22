a6ca4fc98de41e952cd3a6b328b46f1633a2701f
MessyS <1768303852@qq.com>
2019-01-22 14:00:35 +0800

    拾音优化

--- func_messy/volume.py
+++ func_messy/volume.py
@@ -11,7 +11,7 @@
 #                                                                             #
 #     3.RATE设置为48000是为了快速检测，16000的采样率为语音识别的最佳成功率，  #
 #       但是就是会牺牲掉快速的反应能力.....但这里可以通过调小CHUNK来增强反应  #
-#        能力，经测试，200的CHUNK反应速度最快                                 #
+#        能力，经测试，190的CHUNK反应速度最佳                                 #
 #                                                                             #
 ###############################################################################
 '''
@@ -25,7 +25,7 @@ import pygame
 YUZHI = 400
 
 # 保存音频文件的配置
-CHUNK = 180
+CHUNK = 200
 FORMAT = pyaudio.paInt16
 CHANNELS = 1
 RATE = 16000
@@ -33,13 +33,13 @@ RECORD_SECONDS = 5
 WAVE_OUTPUT_FILENAME = "music_messy/said/i_said.wav"
 
 def volume(p):
+	# 睡0.5s防止杂音
 	time.sleep(0.5)
 	stream = p.open(format=FORMAT,
 					channels=CHANNELS,
 					rate=RATE,
 					input=True,
 					frames_per_buffer=CHUNK)
-					
 	# 音频流大小监听
 	# print("      开始监听音频流大小...（5s无应答将结束进程）")
 	frames = []
@@ -54,17 +54,22 @@ def volume(p):
 			
 		if temp > YUZHI:
 			# print('      发现音源输入')
+			panduan = []
 			while 1:
 				for i in range(0, 100):
 					data = stream.read(CHUNK)
 				audio_data = np.fromstring(data, dtype=np.short)
 				temp_in = np.max(audio_data)
 				
-				if temp_in < YUZHI:
-					# print('      音源输入完毕！')
-					stream.stop_stream()
-					stream.close()
-					break
+				if len(panduan) > 3:
+					if temp_in < YUZHI:
+						panduan.append('1')
+				else:
+					if temp_in < YUZHI:
+						# print('      音源输入完毕！')
+						stream.stop_stream()
+						stream.close()
+						break
 			break
 			
 		timing.append('1')
