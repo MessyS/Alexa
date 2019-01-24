491be38defb8d5b4744400ee103a8353002ef41b
MessyS <1768303852@qq.com>
2019-01-24 14:18:04 +0800

    代码细节修改

--- func_messy/logs.py
+++ func_messy/logs.py
@@ -1,18 +1,30 @@
 import traceback
 import time
 
-err_file = 'logs/error.log'
-suc_file = 'logs/success.log'
+err_file = '/opt/MessyPi/yuyin/alexa/logs/error.log'
+suc_file = '/opt/MessyPi/yuyin/alexa/logs/success.log'
+sta_file = '/opt/MessyPi/yuyin/alexa/logs/start.log'
 now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 
 def err():
 	text = traceback.format_exc()
-	logs_text = '[ ' + now_time + ' ] \n' + str(text) + '\n'
+	logs_text = '[ %s ] \n%s\n' % (now_time,text)
+	with open(err_file,'a') as f:
+		f.write(logs_text)
+	return text
+
+def yuyin_err(text):
+	logs_text = '[ %s ] \n%s\n' % (now_time,text)
 	with open(err_file,'a') as f:
 		f.write(logs_text)
 	return text
 	
 def suc(text):
-	logs_text = '[ ' + now_time + ' ]\n    ' + str(text) + '\n\n'
+	logs_text = '[ %s ] \n%s\n\n' % (now_time,text)
 	with open(suc_file,'a') as f:
+		f.write(logs_text)
+
+def start():
+	logs_text = '[ ' + now_time + ' ]   启动成功!\n'
+	with open(sta_file,'a') as f:
 		f.write(logs_text)
--- func_messy/main.py
+++ func_messy/main.py
@@ -10,12 +10,12 @@ import isKeyword
 # 时间
 now_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
 # 提示音频与对话音频的文件路径
-init_prompt = 'music_messy/prompt/how_can_i_help.wav'
-none_prompt = 'music_messy/prompt/please_speak.wav'
-play_shibie_failed = 'music_messy/prompt/shibie_failed.wav'
-play_hecheng_failed = 'music_messy/prompt/hecheng_failed.wav'
-i_said = 'music_messy/said/i_said.wav'
-alexa_said = 'music_messy/said/alexa_said.wav'
+init_prompt = '/opt/MessyPi/yuyin/alexa/music_messy/prompt/how_can_i_help.wav'
+none_prompt = '/opt/MessyPi/yuyin/alexa/music_messy/prompt/please_speak.wav'
+play_shibie_failed = '/opt/MessyPi/yuyin/alexa/music_messy/prompt/shibie_failed.wav'
+play_hecheng_failed = '/opt/MessyPi/yuyin/alexa/music_messy/prompt/hecheng_failed.wav'
+i_said = '/opt/MessyPi/yuyin/alexa/music_messy/said/i_said.wav'
+alexa_said = '/opt/MessyPi/yuyin/alexa/music_messy/said/alexa_said.wav'
 
 # 语音识别引擎（可选）
 # 百度    baidu
@@ -35,7 +35,7 @@ def main(p,detector):
 	# 录音
 	volume_in = volume.volume(p)
 	# 语音识别
-	i_said_json = eval('yuyinshibie.' + shibie_tts + '(i_said)')
+	i_said_json = eval('yuyinshibie.%s(i_said)' % shibie_tts)
 	# 判断是否识别成功(这里我本来想直接用变量的，但不知道为什么第二个if的赋值赋不出来，就先用数组代替)
 	shibie_suc = []
 	if shibie_tts == 'baidu':
@@ -58,15 +58,16 @@ def main(p,detector):
 			# 判断是否包含控制家具关键字
 			if keyword==1 and hecheng_keyword==1:
 				alexa_said_text = tuling.robot(i_said_text)
-				print('      ' + alexa_said_text)
+				print('      %s' % alexa_said_text)
 				# 把我和alexa的对话记录到日志
-				logs.suc('i_said' + i_said_text + '\n    alexa_said:' + alexa_said_text)				
+				logs.suc('i_said%s\n    alexa_said:%s' % i_said_text,alexa_said_text)				
 				# 语音合成阶段的错误处理
-				hecheng = eval('yuyinhecheng.' + hecheng_tts + '(alexa_said_text)')
+				hecheng = eval('yuyinhecheng.%s(alexa_said_text)' % hecheng_tts)
 				if hecheng == 1:
 					volume.play_prompt(alexa_said)
 				else:
+					logs.yuyin_err('语音合成发生了错误，以下是具体信息:\n%s' % hecheng)
 					volume.play_prompt(play_hecheng_failed)
 	else:
-		print('语音识别发生了错误，以下是具体信息:\n', i_said_json)
+		logs.yuyin_err('语音识别发生了错误，以下是具体信息:\n%s' % i_said_json)
 		volume.play_prompt(play_shibie_failed)
